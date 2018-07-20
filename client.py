59f13feb5ef1c8095baa4011debf91a7edf061f9
Aleksey <33554901+AlexeyYan@users.noreply.github.com>
2018-07-20 09:37:25 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,14 +4,13 @@ import getpass
 from auth import *
 from functions import *
 
-scope_list=['friends', 'photos', 'audio', 'video', 'stories', 'status', 'messages', 'wall', 'offline', 'groups', 'email']
+scope_list=['friends', 'photos', 'audio', 'video', 'stories', 'status', 'messages', 'wall', 'offline', 'groups', 'email'
+]
 
 key=input("Token(0) or login(1) authentication: ")
 if key == '0':
     print ("Enter the token: ")
-    input(token)
-    session=vk.Session(vk_token)
-    api = vk.API(session)
+    vk_token=input('Token: ')
 
 elif key =='1':
     email=input("Email or phone: ")
@@ -23,13 +22,37 @@ elif key =='1':
     connect=VKAuth(scope_list, '5970100', '5.80', two_factor_auth=two_factor_auth, email=email, pswd=pswd)
     connect.auth()
     vk_token=connect.get_token()
-    session=vk.Session(vk_token)
-    api = vk.API(session)
-while true:
-    print('1- copy friends')
-    print('2 - get list of conversations')
-    print('3 - send message')
-    print('4 - change the password')
-    print('5 - delete accpunt')
-    key=input('> ')
 
+if vk_token != None and vk_token != '':
+ session=vk.Session(vk_token)
+ api = vk.API(session)
+ r=api.account.getProfileInfo(v='5.80')
+  name=r['first_name']+ '_' + r['last_name']
+ f=open(name+'.txt','w')
+
+ city='City: '+r['city']['title']
+ country='Country: '+r['country']['title']
+ phone='Phone: '+r['phone']
+ home_town='Home town: '+r['home_town']
+ f.write('Account info\n-------------------------\n' + 'Name: ' + name + '\n' + country + ' | ' + city + ' | ' + home_town + '\n' + phone + '\n-------------------------\n')
+ f.close
+ print('OK')
+
+ while True:
+     print('1- copy friends')
+     print('2 - get list of conversations')
+     print('3 - send message')
+     print('4 - change the password')
+     print('5 - delete accpunt')
+     key=input('> ')
+     if key=='1':
+        copyFriends(vk_token)
+     elif key=='2':
+        list_of_Conversations(vk_token)
+     elif key=='3':
+        sendMessage(vk_token)
+     elif key=='4':
+        changePassword(vk_token)
+     elif key=='5':
+        delete()
+        break
